334c86a2a9888c2db7745cb423683f174f31fac3
Dante <dante.ryuuzaki94@gmail.com>
2019-05-10 00:34:51 +0300

    Resolved conflicts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
 public class GameManager : MonoBehaviour {
 
     private static GameManager instance;
-=======
-public class GameManager : MonoBehaviour
-{
-    private static GameManager Instance;
-    
->>>>>>> 924afc0db7f6b01457bed3fa31d052ad2f180cea
     public GameObject player;
     private Player enemy; //is this needed?
     private Graveyard playerGraveyard, enemyGraveyard;
@@ -48,36 +41,9 @@ public class GameManager : MonoBehaviour
         return field.GetEffectValueForType(monsterType);
     }
 
-<<<<<<< HEAD
     public void DrawCard()
     {
         player.GetComponent<Player>().DrawCard();
     }
 
-=======
-    public static GameManager Get()
-    {
-        return Instance;
-    }
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(this);
-            Init();
-        }
-        else
-        {
-            Destroy(gameObject);
-            Debug.Log("GameManager Instance: " + (Instance == this));
-        }
-    }
-
-    private void Init()
-    {
-        GameData.Load();
-    }
->>>>>>> 924afc0db7f6b01457bed3fa31d052ad2f180cea
 }
