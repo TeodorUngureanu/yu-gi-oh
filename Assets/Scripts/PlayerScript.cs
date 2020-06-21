a851d156becfb3ad8db45b1f1cfe7ad06f4fe693
Teodor Ungureanu <teodor.ungureanu@global-technical.com>
2020-06-21 03:00:13 +0300

    graveyard

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
     public GameObject disk, deck, hand;
+    public Canvas GraveyardButtonCanvas;
 
     private long lifePoints;
     private int turnCount;
@@ -44,6 +45,8 @@ public class PlayerScript : MonoBehaviour {
         spellsOnDisk = new List<Card> { null, null, null, null, null };
         quickPlayCards = new HashSet<string>();
         turn = new Turn();
+
+        GraveyardButtonCanvas.gameObject.SetActive(false);
     }
     
     void Start()
@@ -80,6 +83,7 @@ public class PlayerScript : MonoBehaviour {
     {
         yield return new WaitForSeconds(2);
 
+        GraveyardButtonCanvas.gameObject.SetActive(true);
         deck.SetActive(true);
         isReadyForDuel = true;
         UIManager.Get().UpdateDeckSizeOnDisk(deckScript.CardsLeft());
